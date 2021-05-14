5a593717cbfded96e7fcf3e2e2b0d5bdc4da3e5c
박일훈 <chiwoo2074@gmail.com>
2021-05-14 22:59:49 +0900

    feat: initial commit
    프로젝트 생성

---
+++ date-type-study/src/main/java/com/logical/code/study/DateTypeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.logical.code.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateTypeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DateTypeStudyApplication.class, args);
+    }
+
+}
---
+++ date-type-study/src/test/java/com/logical/code/study/DateTypeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logical.code.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTypeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
