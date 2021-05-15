93f3c2c590bdf9975df0c709961e0fefc6c91f4b
박일훈 <chiwoo2074@gmail.com>
2021-05-15 23:28:43 +0900

    feat: resilience4j 커스텀 글로벌 세팅
    1. 커스텀 글로벌 세팅 추가

--- spring-cloud-circuitbreaker-resilience4j/src/test/java/com/logical/code/controller/AlbumControllerTest.java
+++ spring-cloud-circuitbreaker-resilience4j/src/test/java/com/logical/code/controller/AlbumControllerTest.java
@@ -1,6 +1,5 @@
 package com.logical.code.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
