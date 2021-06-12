8594556f473078c02f26636b2765cdf73d7af9fb
박일훈 <chiwoo2074@gmail.com>
2021-06-12 11:40:12 +0900

    feat: Spring Security Study Project setting

--- spring-cloud-circuitbreaker-resilience4j/src/main/java/com/logical/code/StudyApplication.java
+++ spring-cloud-circuitbreaker-resilience4j/src/main/java/com/logical/code/StudyApplication.java
@@ -3,8 +3,10 @@ package com.logical.code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
+@EnableRetry
 @SpringBootApplication
 public class StudyApplication {
 
