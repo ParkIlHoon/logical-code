d31a5c5136f2ce06b57edfa9c80691fe1fc30c84
박일훈 <chiwoo2074@gmail.com>
2021-07-10 22:11:42 +0900

    feat: Job Parameter

--- spring-batch-study/src/main/java/com/logicalcode/study/springbatch/job/SimpleJobConfiguration.java
+++ spring-batch-study/src/main/java/com/logicalcode/study/springbatch/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,15 +24,26 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
     }
 
+    /**
+     *
+     * @param requestDate Spring Batch 가 실행될 때 외부에서 받을 수 있는 파라미터
+     *                    같은 Batch Job 이라도 Job Parameter 가 다르면 Batch_JOB_INSTANCE 테이블에 Job Instance 가 추가된다.
+     *                    Job Parameter 가 동일하면 JobInstanceAlreadyCompleteException 이 발생함.
+     *
+     *                    동일한 Job 이 Job Parameter 가 달라지면 그때마다 BATCH_JOB_INSTANCE 에 생성되며, 동일한 Job Parameter 는 여러개 존재할 수 없음
+     * @return
+     */
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
