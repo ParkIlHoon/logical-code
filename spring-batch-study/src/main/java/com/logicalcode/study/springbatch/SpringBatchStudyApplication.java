e27d05282a1e0eadbf157c652edb54cf06983400
박일훈 <chiwoo2074@gmail.com>
2021-07-10 16:41:34 +0900

    feat: initial commit

---
+++ spring-batch-study/src/main/java/com/logicalcode/study/springbatch/SpringBatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.logicalcode.study.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchStudyApplication.class, args);
+    }
+
+}
