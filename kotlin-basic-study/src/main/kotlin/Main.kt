0e5e26b3f46ad0d265de5b9e10df5794a17d7090
박일훈 <ilhoon.park@nhnsoft.com>
2022-04-11 11:32:38 +0900

    class

--- kotlin-basic-study/src/main/kotlin/Main.kt
+++ kotlin-basic-study/src/main/kotlin/Main.kt
@@ -62,6 +62,31 @@ fun main(args: Array<String>) {
     var instance2 = WithFunClass("하하하", 34)
     instance2.introduce()
     println("---------------------")
+
+    println("---------------------")
+    var instance3 = Person("test")
+    println("---------------------")
+
+    println("---------------------")
+    var dog = Dog("볼트", 3)
+    var cat = Cat("별이", 2)
+    dog.bark()
+    cat.meow()
+    dog.eat()
+    cat.eat()
+    println("---------------------")
+
+    println("---------------------")
+    var tree = Tree()
+    tree.germination()
+    tree.photosynthesis()
+    println("---------------------")
+
+    println("---------------------")
+    var horse = Horse()
+    horse.run()
+    horse.eat()
+    println("---------------------")
 }
 
 /**
@@ -181,3 +206,92 @@ class WithFunClass(val name: String, val age: Int) {
     }
 }
 
+class Person(val name: String, val age: Int) {
+    /**
+     * init : 생성자를 통해 인스턴스가 만들어질 때 호출되는 함수
+     */
+    init {
+        println("${name}은 ${age}살입니다.")
+    }
+    /**
+     * 보조 생성자
+     * 반드시 기본 생성자를 통해 속성을 초기화 해줘야함
+     */
+    constructor(name: String): this(name, 90) {
+        println("보조 생성자가 호출되었습니다.")
+    }
+}
+
+/**
+ * 코틀린은 상속 금지가 기본값(open 상태가 아님)
+ */
+open class Animal(var name: String, var age: Int, var type: String) {
+    fun introduce() {
+        println("저는 ${type} ${name}이고, ${age}살 입니다.")
+    }
+    /**
+     * 오버라이딩 가능
+     */
+    open fun eat() {
+        println("음식을 먹습니다")
+    }
+}
+
+/**
+ * 서브 클래스는 수퍼 클래스에 존재하는 속성과 같은 이름의 속성을 가질 수 없음
+ * 서브 클래스가 생성될 때는 반드시 수퍼클래스의 생성자까지 호출해야함
+ */
+class Dog(name: String, age: Int): Animal(name, age, "개") {
+    fun bark() {
+        println("멍멍")
+    }
+
+    override fun eat() {
+        println("사료를 먹습니다")
+    }
+}
+class Cat(name: String, age: Int): Animal(name, age, "고양이") {
+    fun meow() {
+        println("야옹")
+    }
+}
+
+/**
+ * 추상클래스
+ */
+abstract class Plant {
+    abstract fun germination()
+    fun photosynthesis() {
+        println("광합성을 합니다")
+    }
+}
+class Tree: Plant() {
+    override fun germination() {
+        println("나무가 발아합니다.")
+    }
+}
+
+/**
+ * 인터페이스
+ * 속성, 추상함수, 일반함수
+ * 생성자 없음
+ * 구현부가 있는 함수 -> open 함수
+ * 구현부가 없는 함수 -> abstract 함수
+ */
+interface Runner {
+    fun run()
+}
+interface Eater {
+    fun eat() {
+        println("음식을 먹습니다")
+    }
+}
+class Horse : Runner, Eater {
+    override fun run() {
+        println("다그닥 다그닥")
+    }
+
+    override fun eat() {
+        println("당근을 먹어요")
+    }
+}
