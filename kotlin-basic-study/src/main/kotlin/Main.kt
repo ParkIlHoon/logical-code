729db658bda4416d96556e1d56775513a037c4c2
박일훈 <ilhoon.park@nhnsoft.com>
2022-04-11 16:25:43 +0900

    generic

--- kotlin-basic-study/src/main/kotlin/Main.kt
+++ kotlin-basic-study/src/main/kotlin/Main.kt
@@ -141,6 +141,29 @@ fun main(args: Array<String>) {
     EventPrinter().start()
     println()
     println("---------------------")
+
+    println("---------------------")
+    var drink = Drink()
+    drink.drink()
+
+    var cola: Drink = Cola()
+    cola.drink()
+
+    if (cola is Cola) {
+        cola.washDishes()
+    }
+    var coco = cola as Cola
+    coco.washDishes()
+    cola.washDishes()
+    println("---------------------")
+
+    println("---------------------")
+    UsingGeneric(A()).doShouting()
+    UsingGeneric(B()).doShouting()
+    UsingGeneric(C()).doShouting()
+
+    doShouting(B())
+    println("---------------------")
 }
 
 /**
@@ -460,4 +483,56 @@ class EventCounter(var eventListener: EventListener) {
 }
 interface EventListener {
     fun onEvent(count: Int)
+}
+
+/**
+ * down-casting
+ * as : 변수를 호환되는 자료형으로 변환해주는 캐스팅 연산자. 반환값 뿐만 아니라 변수까지 캐스팅됨
+ * is : 변수가 자료형에 호환되는지를 체크한 후 변환해주는 캐스팅 연산자 (조건문 내에서만 잠시)
+ */
+open class Drink {
+    var name = "음료"
+
+    open fun drink() {
+        println("${name}를 마십니다")
+    }
+}
+class Cola : Drink() {
+    var type = "콜라"
+
+    override fun drink() {
+        println("${name} 중 ${type}를 마십니다")
+    }
+
+    fun washDishes() {
+        println("${type}으로 설거지를 합니다")
+    }
+}
+
+open class A {
+    open fun shout() {
+        println("A 가 소리칩니다")
+    }
+}
+
+class B: A() {
+    override fun shout() {
+        println("B 가 소리칩니다")
+    }
+}
+
+class C: A() {
+    override fun shout() {
+        println("C 가 소리칩니다")
+    }
+}
+
+class UsingGeneric<T: A> (val t: T) {
+    fun doShouting() {
+        t.shout()
+    }
+}
+
+fun <T: A> doShouting(t: T) {
+    t.shout()
 }
