b663308fce8ed88719d30bc9b82c55b62be28351
박일훈 <ilhoon.park@nhnsoft.com>
2022-04-14 14:20:49 +0900

    args and infix function

--- kotlin-basic-study/src/main/kotlin/Main.kt
+++ kotlin-basic-study/src/main/kotlin/Main.kt
@@ -190,6 +190,18 @@ fun main(args: Array<String>) {
     println(prodA == prodD)
     println(prodA === prodD)
     println("---------------------")
+
+    println("---------------------")
+    deliveryItem("짬뽕")
+    deliveryItem("짜장면", 3)
+    deliveryItem("탕수육", 2, "학교")
+    deliveryItem("선물", destination = "친구집")
+    println("---------------------")
+
+    println("---------------------")
+    println(6 multiply 4)
+    println(6.multiply(4))
+    println("---------------------")
 }
 
 /**
@@ -587,3 +599,26 @@ class Product(val name: String, val price: Int) {
         return false
     }
 }
+
+/**
+ * default args
+ */
+fun deliveryItem(name: String, count: Int = 1, destination: String = "집") {
+    println("${name}, ${count} 개를 ${destination} 에 배달하였습니다.")
+}
+
+/**
+ * varargs
+ */
+fun sum(vararg number: Int):Int {
+    var sum = 0;
+    for (n in number) {
+        sum += n
+    }
+    return sum
+}
+
+/**
+ * infix function
+ */
+infix fun Int.multiply(x: Int): Int = this * x
